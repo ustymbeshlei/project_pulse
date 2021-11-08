23a5a28a07420c2f1c345d950813cf111e91d711
Ustim Beshley <ystumys@ukr.net>
2021-11-08 18:32:29 +0200

    added slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        dots: true,
-        speed: 1200,
-        adaptiveHeight: true
-      });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         //adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//             }
+//         }
+//         ]
+        
+//       });
+//   });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    speed: 1200
   });
+
+document.querySelector('.prev').addEventListener('click', function()  {
+    slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function()  {
+    slider.goTo('next');
+});
+
